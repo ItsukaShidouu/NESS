590b1d0f2f29d3f068752b3456157fb3457ab88a
NPC <akuhitaminiakunam@gmail.com>
2025-04-22 23:29:18 +0000

    + Tamplate

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
-import { SoundCloudDl } from './plugins/DL-soundcloud.js';
-import { FBDl } from './plugins/DL-facebook.js';
-import { IGDl } from './plugins/DL-instagram.js';
 import { TTDl } from './plugins/DL-tiktok.js';
-import { XDl } from './plugins/DL-x.js';
-import { youtubeAIO } from './plugins/DL-ytdl.js';
-import { SFWcosplay } from './plugins/ANI-cosplay.js';
-import { SFWlolicon } from './plugins/ANI-loli.js';
-import { SFWaifu } from './plugins/ANI-waifuv1.js';
-import { PinSearch } from './plugins/ANI-pin.js';
-import { ANIwallpaper } from './plugins/ANI-wallpaper.js';
-import { AiInsfxV2 } from './plugins/AI-insfxv2.js';
-import { OTTnew } from './plugins/OTT-new.js';
-import { OTTepid } from './plugins/OTT-epid.js';
-import { OTTsearch } from './plugins/OTT-search.js';
-import { OTTinfo } from './plugins/OTT-info.js';
 
 const nessid = {
-  OTTnew,
-  OTTepid,
-  OTTinfo,
-  OTTsearch,
-  AiInsfxV2,
-  SoundCloudDl,
-  FBDl,
-  IGDl,
-  TTDl,
-  XDl,
-  youtubeAIO,
-  SFWaifu,
-  SFWlolicon,
-  SFWcosplay,
-  PinSearch,
-  ANIwallpaper
+  TTDl
 };
 
 export default nessid;
---
+++ plugins/DL.tiktok.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { domain } from '../index.js';
+
+export async function TTDl(url) {
+  if (!url) throw new Error('URL is required.');
+
+  try {
+    const { data } = await axios.get(`${domain}/api/downloader/tiktok`, {
+      params: { url }, 
+    });
+
+    return data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || error.message);
+  }
+}
