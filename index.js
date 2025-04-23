30436388748bb73814fd7f4dea2eb917a01046ff
YusupKakuu <akuhitaminiakunam@gmail.com>
2025-04-23 19:16:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { AioDl } from './plugins/DL-Aio.js';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const pluginsDir = path.join(__dirname, 'plugins');
-const nessid = {};
-
-// Baca semua file dari folder plugins
-const files = fs.readdirSync(pluginsDir).filter(file => file.endsWith('.js'));
-
-for (const file of files) {
-  const modulePath = path.join(pluginsDir, file);
-  const { default: plugin } = await import(`./plugins/${file}`);
-  
-  // Ambil nama export function dari file
-  const name = path.basename(file).replace('.js', '');
-
-  // Simpan ke objek 'nessid' dengan key sesuai nama file
-  nessid[name] = plugin;
-}
+const nessid = {
+  AioDl
+};
 
 export default nessid;
 export const domain = 'https://backend.ness.biz.id';
