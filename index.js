2deeb24b7272ed1414da7e26a696fccc95af4d29
YusupKakuu <akuhitaminiakunam@gmail.com>
2025-05-10 07:46:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-import { AioDl } from './plugins/DL-aio.js';
-import { TTDl } from './plugins/DL-TTDl.js';
+import { AioDl } from './plugins/DL-AIO.js';
+import { TTDl } from './plugins/DL-TT.js';
+import { IGDl } from './plugins/DL-IG.js';
+import { XDl } from './plugins/DL-X.js';
 
 const nessid = {
   AioDl,
-  TTDl
+  TTDl,
+  IGDl,
+  XDl
 };
 
 export default nessid;
