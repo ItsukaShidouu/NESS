6fcc84b0b42bcb61cfaa6b0ddb772416ec42d79b
YusupKakuu <akuhitaminiakunam@gmail.com>
2025-05-10 08:12:57 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-import nessid from './index.js';
+/*import nessid from './index.js';
 
 nessid.TTDl('https://www.tiktok.com/@dantayspicks/video/7492818112589466923')
   .then(console.log)
   .catch(console.error);
+  */
