0a41efc429053b49cf4e0c62e1eb64bce1478522
YusupKakuu <akuhitaminiakunam@gmail.com>
2025-05-13 00:47:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { AioDl } from './plugins/DL-AIO.js';
 import { TTDl } from './plugins/DL-TT.js';
 import { IGDl } from './plugins/DL-IG.js';
+import { SPOTIFYDl } from './plugins/DL-SPOTIFY.js';
 import { XDl } from './plugins/DL-X.js';
 import { YTMP3Dl } from './plugins/DL-YTMP3.js';
 import { YTMP4Dl } from './plugins/DL-YTMP4.js';
@@ -13,6 +14,7 @@ const nessid = {
   AioDl,
   TTDl,
   IGDl,
+  SPOTIFYDl,
   XDl,
   YTMP3Dl,
   YTMP4Dl,
