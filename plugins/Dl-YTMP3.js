b027e9834cf17a88741bf2eda9986f0c821df27c
YusupKakuu <nessxyz29@gmail.com>
2025-10-04 03:53:23 +0700

    Update and rename DL-YTMP3.js to Dl-YTMP3.js

--- plugins/Dl-YTMP3.js
+++ plugins/Dl-YTMP3.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { domain } from '../index.js';
 
-export async function YTMP3Dl(url) {
+export async function Dl_MP3(url) {
   if (!url) throw new Error('URL is required.');
 
   try {
