2f087dca7b1f8da5d5fcc4772180413d448d7633
YusupKakuu <nessxyz29@gmail.com>
2025-10-04 03:45:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-import { AioDl } from './plugins/DL-AIO.js';
-import { TTDl } from './plugins/DL-TT.js';
-import { IGDl } from './plugins/DL-IG.js';
-import { SPOTIFYDl } from './plugins/DL-SPOTIFY.js';
-import { XDl } from './plugins/DL-X.js';
-import { YTMP3Dl } from './plugins/DL-YTMP3.js';
-import { YTMP4Dl } from './plugins/DL-YTMP4.js';
-import { AWaifu } from './plugins/ANIME-Waifu.js';
-import { ALoli } from './plugins/ANIME-Loli.js';
-import { ACosplayer } from './plugins/ANIME-Cosplayer.js';
-import { AWallpaper } from './plugins/ANIME-Wallpaper.js';
-import { Dl-ytsearch } from './plugins/Dl-ytsearch.js';
+import { A_Cos } from './plugins/ANI-Cos.js';
+import { A_Loli } from './plugins/ANI-Loli.js';
+import { A_Wall } from './plugins/ANI-Wall.js';
+import { A_Waifu } from './plugins/ANI-Waifu.js';
+import { Dl_AIO } from './plugins/Dl-AIO.js';
+import { Dl_IG } from './plugins/Dl-IG.js';
+import { Dl_MP3 } from './plugins/Dl-YTMP3.js';
+import { Dl_MP4 } from './plugins/Dl-YTMP4.js';
+import { Dl_SPOTIFY } from './plugins/Dl-SPOTIFY.js';
+import { Dl_TT } from './plugins/Dl-TT.js';
+import { Dl_x } from './plugins/Dl-X.js';
+import { Dl_YTS } from './plugins/Dl-yts.js';
 
-const nessid = {
-  AioDl,
-  TTDl,
-  IGDl,
-  SPOTIFYDl,
-  XDl,
-  YTMP3Dl,
-  YTMP4Dl,
-  AWaifu,
-  ALoli,
-  ACosplayer,
-  AWallpaper,
-  Dl-ytsearch
+const ness = {
+  A_Cos,
+  A_Loli,
+  A_Wall,
+  A_Waifu,
+  Dl_AIO,
+  Dl_IG,
+  Dl_MP3,
+  Dl_MP4,
+  Dl_SPOTIFY,
+  Dl_TT,
+  Dl_x,
+  Dl_YTS
 };
 
-export default nessid;
+export default ness;
 export const domain = 'https://api.ness.biz.id';
