6ea4640d2c9e9a1fdd8d7b887eb75976311c149a
YusupKakuu <nessxyz29@gmail.com>
2025-10-04 04:13:24 +0700

    Update Dl-YTS.js

--- plugins/Dl-YTS.js
+++ plugins/Dl-YTS.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import { domain } from '../index.js';
 
-export async function Dl_YTS() {
+export async function Dl_YTS(query) {
+  if (!query) throw new Error('Query pencarian diperlukan!');
+
   try {
-    const { data } = await axios.get(`${domain}/api/downloader/yt-search`);
+    const { data } = await axios.get(`${domain}/api/downloader/yt-search`, {
+      params: { q: query }
+    });
     return data;
   } catch (error) {
     throw new Error(error.response?.data?.message || error.message);
